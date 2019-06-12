3efb4df5b5e7392581819930e7f94f436c38221f
comeme <developer.k.mshr0316@gmail.com>
2019-06-13 08:08:32 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="navbar has-shadow">
+                    <div className="navbar-brand">
+                        <div className="navbar-item">
+                            COMEME
+                        </div>
+                    </div>
+                </header>
+                <section className="hero is-success is-fullheight">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Masahiro Komeda
+                            </h1>
+                            <h2 className="subtitle">
+                                comeme
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
